8523673c9da1a28c2937b0f5d52ed71b802819df
Ademar Alves de Oliveira <ademar.oliveira@wizeline.com>
2017-11-28 09:22:18 -0600

    Add an example of how to use the same core on unix

---
+++ Unix/Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include "../CPP/Core.h"
+
+int main() {
+  std::string textFromCppCore = concateneMyStringWithCppString("Unix");
+  std::cout << textFromCppCore << '\n';
+  return 0;
+}
---
+++ Unix/compileToUnix.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+g++ Main.cpp ../CPP/Core.cpp -o main
+echo "Compiled, now execute using ./main"
